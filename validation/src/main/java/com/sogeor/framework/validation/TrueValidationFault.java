ef280557d5d040283aa2baeda806a329a85397ac
George <bloogefest@gmail.com>
2025-01-19 22:55:40 +0300

    Удаление поля `TrueValidationFault.TEMPLATE_MESSAGE`

--- validation/src/main/java/com/sogeor/framework/validation/TrueValidationFault.java
+++ validation/src/main/java/com/sogeor/framework/validation/TrueValidationFault.java
@@ -37,13 +37,6 @@ public class TrueValidationFault extends ValidationFault {
     public static final @NonNull String DEFAULT_MESSAGE = "The value must be false";
 
     /**
-     * Содержит шаблонное сообщение.
-     *
-     * @since 1.0.0-RC1
-     */
-    public static final @NonNull String TEMPLATE_MESSAGE = "%s must be false";
-
-    /**
      * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, {@linkplain #DEFAULT_CAUSE причиной возникновения},
      * параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по
      * умолчанию.
