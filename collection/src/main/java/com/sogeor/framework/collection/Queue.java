11ee3dfef8d772e2fb8b797334c72bc80ba9af2d
George <bloogefest@gmail.com>
2025-01-21 17:14:37 +0300

    Введение интерфейса `Queue`

---
+++ collection/src/main/java/com/sogeor/framework/collection/Queue.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой очередь элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see QueueIterator
+ * @since 1.0.0-RC1
+ */
+public interface Queue<T> extends Collection<T> {
+
+    /**
+     * @return Итератор элементов.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> new")
+    @NonNull
+    QueueIterator<T> iterator();
+
+    /**
+     * @return Особенности очереди.
+     *
+     * @see StandardCollectionFeature
+     * @see StandardQueueFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    @NonNull
+    ImmutableSet<@NonNull CollectionFeature> features();
+
+}
