3f98b0c578d54ae695825eb0a171ef0873d7e823
George <bloogefest@gmail.com>
2025-01-21 16:20:35 +0300

    Введение класса `AbstractSetIterator`

---
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractSetIterator.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой абстрактный итератор элементов множества.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see AbstractSet
+ * @since 1.0.0-RC1
+ */
+public abstract class AbstractSetIterator<T> extends AbstractIterator<T> implements SetIterator<T> {
+
+    /**
+     * Создаёт экземпляр на основе {@code features}.
+     *
+     * @param features особенности итератора.
+     *
+     * @since 1.0.0-RC1
+     */
+    protected AbstractSetIterator(final @NonNull ImmutableSet<@NonNull IteratorFeature> features) {
+        super(features);
+    }
+
+    /**
+     * @return Особенности итератора.
+     *
+     * @see StandardIteratorFeature
+     * @see StandardSetIteratorFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    public @NonNull ImmutableSet<@NonNull IteratorFeature> features() {
+        return super.features();
+    }
+
+}
