0a8425a8018bfc131d14b8d93805fe14b2f573c8
George <bloogefest@gmail.com>
2025-01-21 17:04:54 +0300

    Введение класса `AbstractStackIterator`

---
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractStackIterator.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой абстрактный итератор элементов стека.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see AbstractStack
+ * @since 1.0.0-RC1
+ */
+public abstract class AbstractStackIterator<T> extends AbstractIterator<T> implements StackIterator<T> {
+
+    /**
+     * Создаёт экземпляр на основе {@code features}.
+     *
+     * @param features особенности итератора.
+     *
+     * @since 1.0.0-RC1
+     */
+    protected AbstractStackIterator(final @NonNull ImmutableSet<@NonNull IteratorFeature> features) {
+        super(features);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #end()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    public abstract @NonNull AbstractStackIterator<T> start();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #next()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    public abstract @NonNull AbstractStackIterator<T> previous();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #previous()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    public abstract @NonNull AbstractStackIterator<T> next();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #start()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    public abstract @NonNull AbstractStackIterator<T> end();
+
+    /**
+     * @return Особенности итератора.
+     *
+     * @see StandardIteratorFeature
+     * @see StandardStackIteratorFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    public @NonNull ImmutableSet<@NonNull IteratorFeature> features() {
+        return super.features();
+    }
+
+}
