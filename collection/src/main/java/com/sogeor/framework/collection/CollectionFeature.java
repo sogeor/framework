cc6e033afb11a80f40eb820ded3709d43c70b056
George <bloogefest@gmail.com>
2025-01-21 16:11:19 +0300

    Изменение интерфейса `CollectionFeature`

--- collection/src/main/java/com/sogeor/framework/collection/CollectionFeature.java
+++ collection/src/main/java/com/sogeor/framework/collection/CollectionFeature.java
@@ -20,14 +20,14 @@ import com.sogeor.framework.annotation.Contract;
 import com.sogeor.framework.annotation.NonNull;
 
 /**
- * Представляет собой особенность (1) коллекции элементов.
+ * Представляет собой особенность коллекции элементов.
  *
  * @since 1.0.0-RC1
  */
 public interface CollectionFeature {
 
     /**
-     * @return Имя {1}.
+     * @return Имя особенности.
      *
      * @since 1.0.0-RC1
      */
