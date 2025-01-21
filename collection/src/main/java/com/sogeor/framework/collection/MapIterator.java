041a6067167a349bf7f03607d74d77a03da3ba7a
George <bloogefest@gmail.com>
2025-01-21 22:21:41 +0300

    Введение интерфейса `MapIterator`

---
+++ collection/src/main/java/com/sogeor/framework/collection/MapIterator.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой итератор элементов ассоциативного массива — пар, каждая из которых состоит из ключа и
+ * соответствующего ему значения.
+ *
+ * @param <K> тип ключей.
+ * @param <V> тип значений.
+ * @param <T> тип элементов.
+ *
+ * @see Map
+ * @see MapEntry
+ * @since 1.0.0-RC1
+ */
+public interface MapIterator<K, V, T extends MapEntry<K, V>> extends Iterator<T> {
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #end()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    MapIterator<K, V, T> start();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #previous()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    MapIterator<K, V, T> previous();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #start()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    MapIterator<K, V, T> next();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #last()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    MapIterator<K, V, T> end();
+
+    /**
+     * @return Особенности итератора.
+     *
+     * @see StandardIteratorFeature
+     * @see StandardMapIteratorFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    @NonNull
+    ImmutableSet<@NonNull IteratorFeature> features();
+
+}
