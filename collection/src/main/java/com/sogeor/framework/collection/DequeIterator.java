70e5cc99a6cdc767abc3511cac6735f070da7af6
George <bloogefest@gmail.com>
2025-01-21 17:14:30 +0300

    Введение интерфейса `DequeIterator`

---
+++ collection/src/main/java/com/sogeor/framework/collection/DequeIterator.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой итератор элементов двусторонней очереди.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see Deque
+ * @since 1.0.0-RC1
+ */
+public interface DequeIterator<T> extends Iterator<T> {
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #end()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    DequeIterator<T> start();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #previous()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    DequeIterator<T> previous();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #start()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    DequeIterator<T> next();
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code this}.
+     *
+     * @see #last()
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> this")
+    @NonNull
+    DequeIterator<T> end();
+
+    /**
+     * @return Особенности итератора.
+     *
+     * @see StandardIteratorFeature
+     * @see StandardDequeIteratorFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    @NonNull
+    ImmutableSet<@NonNull IteratorFeature> features();
+
+}
