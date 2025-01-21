04e53638fbb64f3c6c8f6fd5ac84c0194a52c5e9
George <bloogefest@gmail.com>
2025-01-21 16:19:53 +0300

    Введение класса `AbstractCollection`

---
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractCollection.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.immutable.ImmutableSet;
+
+/**
+ * Представляет собой абстрактную коллекцию элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see AbstractIterator
+ * @since 1.0.0-RC1
+ */
+public abstract class AbstractCollection<T> implements Collection<T> {
+
+    /**
+     * Содержит особенности коллекции.
+     *
+     * @since 1.0.0-RC1
+     */
+    private final @NonNull ImmutableSet<@NonNull CollectionFeature> features;
+
+    /**
+     * Создаёт экземпляр на основе {@code features}.
+     *
+     * @param features особенности коллекции.
+     *
+     * @since 1.0.0-RC1
+     */
+    protected AbstractCollection(final @NonNull ImmutableSet<@NonNull CollectionFeature> features) {
+        this.features = features;
+    }
+
+    /**
+     * @return Особенности коллекции.
+     *
+     * @see StandardCollectionFeature
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> $!null")
+    public @NonNull ImmutableSet<@NonNull CollectionFeature> features() {
+        return features;
+    }
+
+}
