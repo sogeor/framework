8603e825968220e8314bd6ab241cb59bb301fa8c
George <bloogefest@gmail.com>
2025-01-22 16:03:51 +0300

    Изменение пакета `com.sogeor.framework.collection`

--- collection/src/main/java/com/sogeor/framework/collection/package-info.java
+++ collection/src/main/java/com/sogeor/framework/collection/package-info.java
@@ -24,54 +24,20 @@
  * @see com.sogeor.framework.collection.writable
  * @see com.sogeor.framework.collection.AbstractCollection
  * @see com.sogeor.framework.collection.AbstractDeque
- * @see com.sogeor.framework.collection.AbstractDequeIterator
- * @see com.sogeor.framework.collection.AbstractIterator
  * @see com.sogeor.framework.collection.AbstractList
- * @see com.sogeor.framework.collection.AbstractListIterator
  * @see com.sogeor.framework.collection.AbstractMultiset
- * @see com.sogeor.framework.collection.AbstractMultisetIterator
  * @see com.sogeor.framework.collection.AbstractQueue
- * @see com.sogeor.framework.collection.AbstractQueueIterator
  * @see com.sogeor.framework.collection.AbstractSet
- * @see com.sogeor.framework.collection.AbstractSetIterator
  * @see com.sogeor.framework.collection.AbstractStack
- * @see com.sogeor.framework.collection.AbstractStackIterator
  * @see com.sogeor.framework.collection.Collection
  * @see com.sogeor.framework.collection.CollectionFault
- * @see com.sogeor.framework.collection.CollectionFeature
  * @see com.sogeor.framework.collection.Deque
- * @see com.sogeor.framework.collection.DequeIterator
- * @see com.sogeor.framework.collection.Iterator
- * @see com.sogeor.framework.collection.IteratorFeature
  * @see com.sogeor.framework.collection.List
- * @see com.sogeor.framework.collection.ListIterator
  * @see com.sogeor.framework.collection.Map
- * @see com.sogeor.framework.collection.MapEntry
- * @see com.sogeor.framework.collection.MapIterator
  * @see com.sogeor.framework.collection.Multiset
- * @see com.sogeor.framework.collection.MultisetIterator
  * @see com.sogeor.framework.collection.Queue
- * @see com.sogeor.framework.collection.QueueIterator
  * @see com.sogeor.framework.collection.Set
- * @see com.sogeor.framework.collection.SetIterator
  * @see com.sogeor.framework.collection.Stack
- * @see com.sogeor.framework.collection.StackIterator
- * @see com.sogeor.framework.collection.StandardCollectionFeature
- * @see com.sogeor.framework.collection.StandardDequeFeature
- * @see com.sogeor.framework.collection.StandardDequeIteratorFeature
- * @see com.sogeor.framework.collection.StandardIteratorFeature
- * @see com.sogeor.framework.collection.StandardListFeature
- * @see com.sogeor.framework.collection.StandardListIteratorFeature
- * @see com.sogeor.framework.collection.StandardMapFeature
- * @see com.sogeor.framework.collection.StandardMapIteratorFeature
- * @see com.sogeor.framework.collection.StandardMultisetFeature
- * @see com.sogeor.framework.collection.StandardMultisetIteratorFeature
- * @see com.sogeor.framework.collection.StandardQueueFeature
- * @see com.sogeor.framework.collection.StandardQueueIteratorFeature
- * @see com.sogeor.framework.collection.StandardSetFeature
- * @see com.sogeor.framework.collection.StandardSetIteratorFeature
- * @see com.sogeor.framework.collection.StandardStackFeature
- * @see com.sogeor.framework.collection.StandardStackIteratorFeature
  * @since 1.0.0-RC1
  */
 package com.sogeor.framework.collection;
