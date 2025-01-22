16b7dc2dc926637053eb8875b6a78399a3d2c579
George <bloogefest@gmail.com>
2025-01-22 16:22:30 +0300

    Изменение класса `AbstractQueue.AbstractIterator`

--- collection/src/main/java/com/sogeor/framework/collection/AbstractQueue.java
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractQueue.java
@@ -41,6 +41,8 @@ public abstract class AbstractQueue<T> extends AbstractCollection<T> implements 
      *
      * @param <T> тип элементов.
      *
+     * @implSpec Каждый итератор должен быть способен переходить к элементу, расположенному либо перед текущим, либо
+     * после него, либо к обоим из них.
      * @see AbstractQueue
      * @since 1.0.0-RC1
      */
