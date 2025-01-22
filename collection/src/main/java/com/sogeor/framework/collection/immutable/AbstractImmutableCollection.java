dccc7ebf08ca9906ed2f244f901368eb6a2a9a38
George <bloogefest@gmail.com>
2025-01-22 20:58:10 +0300

    Изменение класса `AbstractImmutableCollection`

---
+++ collection/src/main/java/com/sogeor/framework/collection/immutable/AbstractImmutableCollection.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection.immutable;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.readable.AbstractReadableCollection;
+import com.sogeor.framework.function.Consumer;
+import com.sogeor.framework.validation.NullValidationFault;
+
+/**
+ * Представляет собой абстрактную неизменяемую коллекцию элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see AbstractIterator
+ * @since 1.0.0-RC1
+ */
+public abstract class AbstractImmutableCollection<T> extends AbstractReadableCollection<T> implements ImmutableCollection<T> {
+
+    /**
+     * Создаёт экземпляр.
+     *
+     * @since 1.0.0-RC1
+     */
+    protected AbstractImmutableCollection() {}
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param consumer потребитель элементов.
+     *
+     * @return {@code this}.
+     *
+     * @throws NullValidationFault {@code consumer} не должен быть {@code null}.
+     * @throws F неудачное потребление элемента с помощью {@code consumer}.
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("!null -> this; null -> fault")
+    public <F extends Throwable> @NonNull AbstractImmutableCollection<T> iterate(
+            final @NonNull Consumer<? super T, F> consumer) throws NullValidationFault, F {
+        super.iterate(consumer);
+        return this;
+    }
+
+    /**
+     * Представляет собой абстрактный итератор элементов неизменяемой коллекции.
+     *
+     * @param <T> тип элементов.
+     *
+     * @implSpec Каждый итератор должен быть способен переходить к элементу, расположенному либо перед текущим, либо
+     * после него, либо к обоим из них.
+     * <p>
+     * Если итератор способен переходить к элементу, расположенному перед текущим, то он должен быть также способен
+     * переходить к последнему. И наоборот, если итератор способен переходить к элементу, расположенному после текущего,
+     * то он должен быть также способен переходить к первому. Это необходимо для корректной итерации, например:
+     * <pre>
+     * {@code
+     * void example(final @NonNull Iterator<?> it) {
+     *     if (it.canNext()) { // it.canStart() == true
+     *         for (it.start(); it.after(); it.next()) {
+     *             // ...
+     *         }
+     *     } else { // it.canPrevious() == true && it.canEnd() == true
+     *         for (it.end(); it.before(); it.previous()) {
+     *             // ...
+     *         }
+     *     }
+     * }
+     * }
+     * </pre>
+     * @see AbstractImmutableCollection
+     * @since 1.0.0-RC1
+     */
+    public abstract static class AbstractIterator<T> extends AbstractReadableCollection.AbstractIterator<T> implements ImmutableCollection.Iterator<T> {
+
+        /**
+         * Создаёт экземпляр.
+         *
+         * @since 1.0.0-RC1
+         */
+        protected AbstractIterator() {}
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #end()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<T> start();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #next()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<T> previous();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #previous()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<T> next();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #start()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<T> end();
+
+    }
+
+}
