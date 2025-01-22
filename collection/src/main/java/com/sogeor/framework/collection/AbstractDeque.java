aeee111c61122256eb85972ab0929e4bafab6e7c
George <bloogefest@gmail.com>
2025-01-22 16:22:53 +0300

    Изменение класса `AbstractDeque.AbstractIterator`

--- collection/src/main/java/com/sogeor/framework/collection/AbstractDeque.java
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractDeque.java
@@ -41,6 +41,8 @@ public abstract class AbstractDeque<T> extends AbstractCollection<T> implements 
      *
      * @param <T> тип элементов.
      *
+     * @implSpec Каждый итератор должен быть способен переходить к элементу, расположенному либо перед текущим, либо
+     * после него, либо к обоим из них.
      * @see AbstractDeque
      * @since 1.0.0-RC1
      */
