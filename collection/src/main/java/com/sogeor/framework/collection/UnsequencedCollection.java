335467e73099844c27b63a3fee310d14eb462279
George <bloogefest@gmail.com>
2025-02-15 19:56:10 +0300

    Введение интерфейса `UnsequencedCollection`

---
+++ collection/src/main/java/com/sogeor/framework/collection/UnsequencedCollection.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+
+/**
+ * Представляет собой неупорядоченную коллекцию элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see Iterator
+ * @since 1.0.0-RC1
+ */
+public interface UnsequencedCollection<T> extends Collection<T> {
+
+    /**
+     * @return Итератор элементов этой неупорядоченной коллекции.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> new")
+    @NonNull
+    Iterator<T> iterator();
+
+    /**
+     * Представляет собой итератор элементов неупорядоченной коллекции.
+     *
+     * @param <T> тип элементов.
+     *
+     * @see UnsequencedCollection
+     * @since 1.0.0-RC1
+     */
+    interface Iterator<T> extends Collection.Iterator<T> {
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #end()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> start();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #previous()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> previous();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #start()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> next();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #last()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> end();
+
+    }
+
+}
