1d5eac97809ecfe7b1bd1694d82deced2d918c55
George <bloogefest@gmail.com>
2025-02-24 22:35:49 +0300

    Введение интерфейса `ReadableSequencedCollection`

---
+++ collection/src/main/java/com/sogeor/framework/collection/readable/ReadableSequencedCollection.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection.readable;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.SequencedCollection;
+import com.sogeor.framework.function.Consumer;
+import com.sogeor.framework.validation.NullValidationFault;
+import com.sogeor.framework.validation.ValidationFault;
+
+/**
+ * Представляет собой читаемую упорядоченную коллекцию элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see Iterator
+ * @since 1.0.0-RC1
+ */
+public interface ReadableSequencedCollection<T> extends SequencedCollection<T>, ReadableCollection<T> {
+
+    /**
+     * Потребляет каждый из элементов с помощью {@code consumer}.
+     *
+     * @param consumer потребитель элементов.
+     *
+     * @return {@code this}.
+     *
+     * @throws ValidationFault неудачная валидация.
+     * @throws NullValidationFault {@code consumer} не должен быть {@code null}.
+     * @throws F неудачное потребление элемента с помощью {@code consumer}.
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("!null -> this; null -> fault")
+    default <F extends Throwable> @NonNull ReadableSequencedCollection<T> iterate(
+            final @NonNull Consumer<? super T, F> consumer) throws ValidationFault, F {
+        ReadableCollection.super.iterate(consumer);
+        return this;
+    }
+
+    /**
+     * @return Итератор элементов этой читаемой упорядоченной коллекции.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> new")
+    @NonNull
+    Iterator<T> iterator();
+
+    /**
+     * Представляет собой итератор элементов читаемой упорядоченной коллекции.
+     *
+     * @param <T> тип элементов.
+     *
+     * @see ReadableSequencedCollection
+     * @since 1.0.0-RC1
+     */
+    interface Iterator<T> extends SequencedCollection.Iterator<T>, ReadableCollection.Iterator<T> {
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #first()
+         * @see #canStart()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> start();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #before()
+         * @see #canPrevious()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> previous();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #after()
+         * @see #canNext()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> next();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #last()
+         * @see #canEnd()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        @NonNull
+        Iterator<T> end();
+
+    }
+
+}
