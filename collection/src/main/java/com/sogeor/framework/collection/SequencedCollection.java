c6321d2ff9ee52c231d3c2472891a43c331a1f23
George <bloogefest@gmail.com>
2025-03-05 20:50:32 +0300

    Изменение интерфейса `SequencedCollection`

--- collection/src/main/java/com/sogeor/framework/collection/SequencedCollection.java
+++ collection/src/main/java/com/sogeor/framework/collection/SequencedCollection.java
@@ -116,6 +116,51 @@ public interface SequencedCollection<T> extends Collection<T> {
         @NonNull
         Iterator<T> end();
 
+        /**
+         * Если {@code exists(index)}, то переходит к элементу по {@code index}.
+         *
+         * @param index индекс элемента.
+         *
+         * @return {@code this}.
+         *
+         * @implNote Стандартная реализация обладает оценкой временной сложности {@code O(n)}.
+         * @see #exists(long)
+         * @since 1.0.0-RC1
+         */
+        @Contract("value -> this")
+        default @NonNull Iterator<T> move(final long index) {
+            if (!exists(index)) return this;
+            if (index == index()) return this;
+            while (index > index() && after()) next();
+            while (index < index() && before()) previous();
+            return this;
+        }
+
+        /**
+         * @param index индекс элемента.
+         *
+         * @return Если элемент по {@code index} существует, то {@code true}, иначе {@code false}.
+         *
+         * @implNote Стандартная реализация обладает оценкой временной сложности {@code O(n)}.
+         * @since 1.0.0-RC1
+         */
+        @Contract("value -> value")
+        default boolean exists(final long index) {
+            if (index < 0 || undetermined()) return false;
+            while (index > index() && after()) next();
+            while (index < index() && before()) previous();
+            return index == index();
+        }
+
+        /**
+         * @return Если {@code current()}, то индекс текущего элемента, иначе {@code -1}.
+         *
+         * @see #current()
+         * @since 1.0.0-RC1
+         */
+        @Contract("-> value")
+        long index();
+
     }
 
 }
