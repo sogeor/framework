768460749cb07f0db5f54289b54d3f62dd7a895e
George <bloogefest@gmail.com>
2025-03-08 00:11:08 +0300

    Введение класса `AbstractMap`

---
+++ collection/src/main/java/com/sogeor/framework/collection/AbstractMap.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+
+/**
+ * Представляет собой абстрактный ассоциативный массив элементов — пар, каждая из которых состоит из ключа и
+ * соответствующего ему значения.
+ *
+ * @param <K> тип ключей.
+ * @param <V> тип значений.
+ * @param <T> тип элементов.
+ *
+ * @see AbstractEntry
+ * @see AbstractIterator
+ * @since 1.0.0-RC1
+ */
+public abstract class AbstractMap<K, V, T extends Map.Entry<K, V>> extends AbstractSet<T> implements Map<K, V, T> {
+
+    /**
+     * Создаёт экземпляр.
+     *
+     * @since 1.0.0-RC1
+     */
+    protected AbstractMap() {}
+
+    /**
+     * @return Новый итератор элементов этого ассоциативного массива.
+     *
+     * @implSpec Если {@code !empty()}, то возвращаемый итератор должен находится в определённом состоянии, а также его
+     * текущим элементом должен быть первый элемент этого ассоциативного массива.
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> new")
+    public abstract @NonNull AbstractIterator<K, V, T> iterator();
+
+    /**
+     * Представляет собой абстрактный элемент ассоциативного массива — абстрактную пару, состоящую из ключа и
+     * соответствующего ему значения.
+     *
+     * @param <K> тип ключей.
+     * @param <V> тип значений.
+     *
+     * @see AbstractMap
+     * @see AbstractIterator
+     * @since 1.0.0-RC1
+     */
+    public abstract static class AbstractEntry<K, V> implements Entry<K, V> {
+
+        /**
+         * @return {@code super.toString()}.
+         *
+         * @see Object#toString()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> $value")
+        public @NonNull String toString() {
+            return super.toString();
+        }
+
+    }
+
+    /**
+     * Представляет собой абстрактный итератор элементов абстрактного ассоциативного массива — пар, каждая из которых
+     * состоит из ключа и соответствующего ему значения.
+     *
+     * @param <K> тип ключей.
+     * @param <V> тип значений.
+     * @param <T> тип элементов.
+     *
+     * @see AbstractMap
+     * @see AbstractEntry
+     * @since 1.0.0-RC1
+     */
+    public abstract static class AbstractIterator<K, V, T extends Entry<K, V>> extends AbstractSet.AbstractIterator<T> implements Map.Iterator<K, V, T> {
+
+        /**
+         * Создаёт экземпляр.
+         *
+         * @since 1.0.0-RC1
+         */
+        protected AbstractIterator() {}
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #first()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<K, V, T> start();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #before()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<K, V, T> previous();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #after()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<K, V, T> next();
+
+        /**
+         * {@inheritDoc}
+         *
+         * @return {@code this}.
+         *
+         * @see #last()
+         * @since 1.0.0-RC1
+         */
+        @Override
+        @Contract("-> this")
+        public abstract @NonNull AbstractIterator<K, V, T> end();
+
+    }
+
+}
