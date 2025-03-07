c2c7d7a6274d565a5b2bcfcc699e631fd6e6b0f9
George <bloogefest@gmail.com>
2025-03-08 00:13:47 +0300

    Изменение пакета `com.sogeor.framework.collection`

--- collection/src/main/java/com/sogeor/framework/collection/package-info.java
+++ collection/src/main/java/com/sogeor/framework/collection/package-info.java
@@ -25,19 +25,26 @@
  * @see com.sogeor.framework.collection.AbstractCollection
  * @see com.sogeor.framework.collection.AbstractDeque
  * @see com.sogeor.framework.collection.AbstractList
+ * @see com.sogeor.framework.collection.AbstractMap
+ * @see com.sogeor.framework.collection.AbstractMultimap
  * @see com.sogeor.framework.collection.AbstractMultiset
  * @see com.sogeor.framework.collection.AbstractQueue
+ * @see com.sogeor.framework.collection.AbstractSequencedCollection
  * @see com.sogeor.framework.collection.AbstractSet
  * @see com.sogeor.framework.collection.AbstractStack
+ * @see com.sogeor.framework.collection.AbstractUnsequencedCollection
  * @see com.sogeor.framework.collection.Collection
  * @see com.sogeor.framework.collection.CollectionFault
  * @see com.sogeor.framework.collection.Deque
  * @see com.sogeor.framework.collection.List
  * @see com.sogeor.framework.collection.Map
+ * @see com.sogeor.framework.collection.Multimap
  * @see com.sogeor.framework.collection.Multiset
  * @see com.sogeor.framework.collection.Queue
+ * @see com.sogeor.framework.collection.SequencedCollection
  * @see com.sogeor.framework.collection.Set
  * @see com.sogeor.framework.collection.Stack
+ * @see com.sogeor.framework.collection.UnsequencedCollection
  * @since 1.0.0-RC1
  */
 package com.sogeor.framework.collection;
