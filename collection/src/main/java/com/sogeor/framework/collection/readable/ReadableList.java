97802412910e3810cd07199f47d87a0a8a1a8163
George <bloogefest@gmail.com>
2025-03-25 00:39:02 +0300

    Введение интерфейса `ReadableList`

---
+++ collection/src/main/java/com/sogeor/framework/collection/readable/ReadableList.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.collection.readable;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.collection.List;
+
+/**
+ * Представляет собой читаемый список элементов.
+ *
+ * @param <T> тип элементов.
+ *
+ * @see Iterator
+ * @since 1.0.0-RC1
+ */
+public interface ReadableList<T> extends ReadableSequencedCollection<T>, List<T> {
+
+    /**
+     * @return Новый итератор элементов этого списка.
+     *
+     * @implSpec Если {@code !empty()}, то возвращаемый итератор должен находится в определённом состоянии, а также его
+     * текущим элементом должен быть первый элемент этого списка.
+     * @since 1.0.0-RC1
+     */
+    @Override
+    @Contract("-> new")
+    @NonNull
+    Iterator<T> iterator();
+
+    /**
+     * Представляет собой итератор элементов читаемого списка.
+     *
+     * @param <T> тип элементов.
+     *
+     * @see ReadableList
+     * @since 1.0.0-RC1
+     */
+    interface Iterator<T> extends ReadableSequencedCollection.Iterator<T>, List.Iterator<T> {}
+
+}
