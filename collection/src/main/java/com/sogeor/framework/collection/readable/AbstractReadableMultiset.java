0714acfeab29cfce11ab44f906d7087124a53161
George <bloogefest@gmail.com>
2025-03-25 21:36:24 +0300

    Изменение класса `AbstractReadableMultiset`

--- collection/src/main/java/com/sogeor/framework/collection/readable/AbstractReadableMultiset.java
+++ collection/src/main/java/com/sogeor/framework/collection/readable/AbstractReadableMultiset.java
@@ -19,9 +19,6 @@ package com.sogeor.framework.collection.readable;
 import com.sogeor.framework.annotation.Contract;
 import com.sogeor.framework.annotation.NonNull;
 import com.sogeor.framework.collection.AbstractMultiset;
-import com.sogeor.framework.function.Consumer;
-import com.sogeor.framework.validation.NullValidationFault;
-import com.sogeor.framework.validation.ValidationFault;
 
 /**
  * Представляет собой абстрактное читаемое мультимножество элементов.
@@ -39,27 +36,6 @@ public abstract class AbstractReadableMultiset<T> extends AbstractMultiset<T> im
      * @since 1.0.0-RC1
      */
     protected AbstractReadableMultiset() {}
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param consumer потребитель элементов.
-     *
-     * @return {@code this}.
-     *
-     * @throws ValidationFault неудачная валидация.
-     * @throws NullValidationFault {@code consumer} не должен быть {@code null}.
-     * @throws F неудачное потребление элемента с помощью {@code consumer}.
-     * @implNote Стандартная реализация обладает оценкой временной сложности {@code O(n)}.
-     * @since 1.0.0-RC1
-     */
-    @Override
-    @Contract("!null -> this; null -> fault")
-    public <F extends Throwable> @NonNull AbstractReadableMultiset<T> iterate(
-            final @NonNull Consumer<? super T, F> consumer) throws ValidationFault, F {
-        ReadableMultiset.super.iterate(consumer);
-        return this;
-    }
 
     /**
      * @return Новый итератор элементов этого мультимножества.
@@ -88,54 +64,6 @@ public abstract class AbstractReadableMultiset<T> extends AbstractMultiset<T> im
          * @since 1.0.0-RC1
          */
         protected AbstractIterator() {}
-
-        /**
-         * {@inheritDoc}
-         *
-         * @return {@code this}.
-         *
-         * @see #first()
-         * @since 1.0.0-RC1
-         */
-        @Override
-        @Contract("-> this")
-        public abstract @NonNull AbstractIterator<T> start();
-
-        /**
-         * {@inheritDoc}
-         *
-         * @return {@code this}.
-         *
-         * @see #before()
-         * @since 1.0.0-RC1
-         */
-        @Override
-        @Contract("-> this")
-        public abstract @NonNull AbstractIterator<T> previous();
-
-        /**
-         * {@inheritDoc}
-         *
-         * @return {@code this}.
-         *
-         * @see #after()
-         * @since 1.0.0-RC1
-         */
-        @Override
-        @Contract("-> this")
-        public abstract @NonNull AbstractIterator<T> next();
-
-        /**
-         * {@inheritDoc}
-         *
-         * @return {@code this}.
-         *
-         * @see #last()
-         * @since 1.0.0-RC1
-         */
-        @Override
-        @Contract("-> this")
-        public abstract @NonNull AbstractIterator<T> end();
 
     }
 
