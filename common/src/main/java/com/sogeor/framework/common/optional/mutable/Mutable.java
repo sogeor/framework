43b6c9b4b3bbb05b30166016a082d89ab680b3f8
George <bloogefest@gmail.com>
2025-04-27 22:49:29 +0300

    Изменение класса `Mutable`

--- common/src/main/java/com/sogeor/framework/common/optional/mutable/Mutable.java
+++ common/src/main/java/com/sogeor/framework/common/optional/mutable/Mutable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Sogeor
+ * Copyright 2025 Sogeor
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
-package com.sogeor.framework.common.mutable;
+package com.sogeor.framework.common.optional.mutable;
 
 import com.sogeor.framework.annotation.Contract;
 import com.sogeor.framework.annotation.NonNull;
 import com.sogeor.framework.annotation.Nullable;
+import com.sogeor.framework.common.optional.Optional;
 import com.sogeor.framework.function.Action;
 import com.sogeor.framework.function.Consumer;
 import com.sogeor.framework.function.Handler;
@@ -27,18 +28,20 @@ import com.sogeor.framework.validation.NullValidationFault;
 import com.sogeor.framework.validation.ValidationFault;
 import com.sogeor.framework.validation.Validator;
 
-import java.util.Objects;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Представляет собой изменяемую обёртку над объектом.
+ * <p>
+ * Изменяемая обёртка основана на паре блокировок для чтения и записи, из-за чего она способна обеспечить правильный
+ * доступ к объекту и его существованию.
  *
  * @param <T> тип объекта.
  *
  * @since 1.0.0-RC1
  */
-public final class Mutable<T> {
+public final class Mutable<T> extends Optional<T> {
 
     /**
      * Содержит пару блокировок для чтения и записи.
@@ -48,7 +51,7 @@ public final class Mutable<T> {
     private final @NonNull ReadWriteLock lock = new ReentrantReadWriteLock();
 
     /**
-     * Содержит объект.
+     * Содержит текущий объект.
      *
      * @since 1.0.0-RC1
      */
@@ -57,7 +60,6 @@ public final class Mutable<T> {
     /**
      * Создаёт экземпляр на основе {@code null}.
      *
-     * @see #Mutable(Object)
      * @since 1.0.0-RC1
      */
     @Contract("-> new")
@@ -70,22 +72,20 @@ public final class Mutable<T> {
      *
      * @param object объект.
      *
-     * @see #Mutable()
      * @since 1.0.0-RC1
      */
-    @Contract("$? -> new")
+    @Contract("? -> new")
     private Mutable(final @Nullable T object) {
         this.object = object;
     }
 
     /**
-     * Создаёт и возвращает изменяемую обёртку над {@code null}.
+     * Создаёт изменяемую обёртку над {@code null}.
      *
      * @param <T> тип {@code null}.
      *
      * @return Новую изменяемую обёртку над {@code null}.
      *
-     * @see #of(Object)
      * @since 1.0.0-RC1
      */
     @Contract("-> new")
@@ -94,27 +94,28 @@ public final class Mutable<T> {
     }
 
     /**
-     * Создаёт и возвращает изменяемую обёртку над {@code object}.
+     * Создаёт изменяемую обёртку над {@code object}.
      *
      * @param object объект.
      * @param <T> тип {@code object}.
      *
      * @return Новую изменяемую обёртку над {@code object}.
      *
-     * @see #empty()
      * @since 1.0.0-RC1
      */
-    @Contract("$? -> new")
+    @Contract("? -> new")
     public static <T> @NonNull Mutable<T> of(final @Nullable T object) {
         return new Mutable<>(object);
     }
 
     /**
-     * @return {@code this.object}.
+     * @return {@code object}.
      *
+     * @see #object
      * @since 1.0.0-RC1
      */
-    @Contract("-> ?")
+    @Override
+    @Contract("-> value")
     public @Nullable T get() {
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
@@ -130,11 +131,10 @@ public final class Mutable<T> {
      *
      * @return {@code this}.
      *
-     * @see #set(Handler)
-     * @see #set(Supplier)
+     * @see #object
      * @since 1.0.0-RC1
      */
-    @Contract("$? -> this")
+    @Contract("? -> this")
     public @NonNull Mutable<T> set(final @Nullable T object) {
         final @NonNull var lock = this.lock.writeLock();
         lock.lock();
@@ -144,22 +144,21 @@ public final class Mutable<T> {
     }
 
     /**
-     * Задаёт {@code this.object} равным {@code handler.handle(get())}.
+     * Задаёт {@code object} равным {@code handler.handle(object)}.
      *
      * @param handler обработчик объектов.
      * @param <F> тип программного сбоя или неисправности, возникающей при неудачной обработке или возврате объектов
-     * {@code supplier}.
+     * {@code handler}.
      *
      * @return {@code this}.
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code handler} не должен быть {@code null}.
+     * @see #object
      * @see Handler#handle(Object)
-     * @see #set(Object)
-     * @see #set(Supplier)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> set(
             final @NonNull Handler<? super T, ? extends T, F> handler) throws ValidationFault, F {
         Validator.nonNull(handler, "The passed handler");
@@ -174,7 +173,7 @@ public final class Mutable<T> {
     }
 
     /**
-     * Задаёт {@code this.object} равным {@code supplier.get()}.
+     * Задаёт {@code object} равным {@code supplier.get()}.
      *
      * @param supplier поставщик объектов.
      * @param <F> тип программного сбоя или неисправности, возникающей при неудачной поставке объектов
@@ -184,11 +183,11 @@ public final class Mutable<T> {
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code supplier} не должен быть {@code null}.
+     * @see #object
      * @see Supplier#get()
-     * @see #set(Object)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> set(final @NonNull Supplier<? extends T, F> supplier) throws
                                                                                                            ValidationFault,
                                                                                                            F {
@@ -204,39 +203,39 @@ public final class Mutable<T> {
     }
 
     /**
-     * @return Если {@code get() == null}, то {@code true}, иначе {@code false}.
+     * @return {@code object == null}.
      *
-     * @see #get()
-     * @see #present()
+     * @see #object
      * @since 1.0.0-RC1
      */
-    @Contract("-> ?")
+    @Override
+    @Contract("-> value")
     public boolean absent() {
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        final @Nullable var result = get() == null;
+        final @Nullable var result = object == null;
         lock.unlock();
         return result;
     }
 
     /**
-     * @return Если {@code get() != null}, то {@code true}, иначе {@code false}.
+     * @return {@code object != null}.
      *
-     * @see #get()
-     * @see #absent()
+     * @see #object
      * @since 1.0.0-RC1
      */
-    @Contract("-> ?")
+    @Override
+    @Contract("-> value")
     public boolean present() {
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        final @Nullable var result = get() != null;
+        final @Nullable var result = object != null;
         lock.unlock();
         return result;
     }
 
     /**
-     * Если {@code absent()}, то выполняет {@code action}.
+     * {@inheritDoc}
      *
      * @param action действие.
      * @param <F> тип программного сбоя или неисправности, возникающей во время выполнения {@code action}.
@@ -245,59 +244,59 @@ public final class Mutable<T> {
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code action} не должно быть {@code null}.
-     * @see Action#perform()
+     * @see Action
      * @see #absent()
-     * @see #absent(Consumer)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Override
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> absent(final @NonNull Action<F> action) throws ValidationFault, F {
         Validator.nonNull(action, "The passed action");
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        if (absent()) try {
+        if (present()) lock.unlock();
+        else try {
             action.perform();
         } finally {
             lock.unlock();
         }
-        else lock.unlock();
         return this;
     }
 
     /**
-     * Если {@code absent()}, то выполняет метод {@code consumer.consume(null)}.
+     * {@inheritDoc}
      *
      * @param consumer потребитель объектов.
-     * @param <F> тип программного сбоя или неисправности, возникающей при неудачном потреблении объектов
+     * @param <F> тип программного сбоя или неисправности, возникающей при неудачном потреблении {@code null} с помощью
      * {@code consumer}.
      *
      * @return {@code this}.
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code consumer} не должен быть {@code null}.
-     * @see Consumer#consume(Object)
+     * @see Consumer
      * @see #absent()
-     * @see #absent(Action)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Override
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> absent(final @NonNull Consumer<? super T, F> consumer) throws
                                                                                                             ValidationFault,
                                                                                                             F {
         Validator.nonNull(consumer, "The passed consumer");
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        if (absent()) try {
+        if (present()) lock.unlock();
+        else try {
             consumer.consume(null);
         } finally {
             lock.unlock();
         }
-        else lock.unlock();
         return this;
     }
 
     /**
-     * Если {@code present()}, то выполняет {@code action}.
+     * {@inheritDoc}
      *
      * @param action действие.
      * @param <F> тип программного сбоя или неисправности, возникающей во время выполнения {@code action}.
@@ -306,18 +305,19 @@ public final class Mutable<T> {
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code action} не должно быть {@code null}.
-     * @see Action#perform()
+     * @see Action
      * @see #present()
-     * @see #present(Consumer)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Override
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> present(final @NonNull Action<F> action) throws ValidationFault,
                                                                                                      F {
         Validator.nonNull(action, "The passed action");
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        if (present()) try {
+        if (absent()) lock.unlock();
+        else try {
             action.perform();
         } finally {
             lock.unlock();
@@ -326,75 +326,36 @@ public final class Mutable<T> {
     }
 
     /**
-     * Если {@code present()}, то выполняет метод {@code consumer.consume(get())}.
+     * {@inheritDoc}
      *
      * @param consumer потребитель объектов.
-     * @param <F> тип программного сбоя или неисправности, возникающей при неудачном потреблении объектов
+     * @param <F> тип программного сбоя или неисправности, возникающей при неудачном потреблении {@code get()} с помощью
      * {@code consumer}.
      *
      * @return {@code this}.
      *
      * @throws ValidationFault неудачная валидация.
      * @throws NullValidationFault {@code consumer} не должен быть {@code null}.
+     * @see Consumer
      * @see #get()
-     * @see Consumer#consume(Object)
      * @see #present()
-     * @see #present(Action)
      * @since 1.0.0-RC1
      */
-    @Contract("$!null -> this; null -> fault")
+    @Override
+    @Contract("!null -> this; null -> fault")
     public <F extends Throwable> @NonNull Mutable<T> present(final @NonNull Consumer<? super T, F> consumer) throws
                                                                                                              ValidationFault,
                                                                                                              F {
         Validator.nonNull(consumer, "The passed consumer");
         final @NonNull var lock = this.lock.readLock();
         lock.lock();
-        if (present()) try {
+        if (absent()) lock.unlock();
+        else try {
             consumer.consume(get());
         } finally {
             lock.unlock();
         }
-        else lock.unlock();
         return this;
-    }
-
-    /**
-     * @return {@code Objects.hashCode(get())}.
-     *
-     * @see #get()
-     * @see Objects#hashCode(Object)
-     * @since 1.0.0-RC1
-     */
-    @Override
-    @Contract("-> value")
-    public int hashCode() {
-        return Objects.hashCode(get());
-    }
-
-    /**
-     * @param object объект.
-     *
-     * @return Если {@code this} равно {@code object}, то {@code true}, иначе {@code false}.
-     *
-     * @see #get()
-     * @see Objects#equals(Object, Object)
-     * @since 1.0.0-RC1
-     */
-    @Override
-    @Contract("-> value")
-    public boolean equals(final @Nullable Object object) {
-        return object instanceof final @NonNull Mutable<?> that && Objects.equals(get(), that.get());
-    }
-
-    /**
-     * @return Строковое представление {@code this}.
-     *
-     * @since 1.0.0-RC1
-     */
-    @Override
-    @Contract("-> value")
-    public String toString() {
-        return getClass().getSimpleName() + '@' + Integer.toHexString(hashCode()) + "{object=" + object + '}';
     }
 
 }
