36091f9fc97203232bf1488fcddb0fe1ed880086
George <bloogefest@gmail.com>
2025-05-01 22:43:58 +0300

    Изменение пакета `com.sogeor.framework.common.optional.mutable`

--- common/src/main/java/com/sogeor/framework/common/optional/mutable/package-info.java
+++ common/src/main/java/com/sogeor/framework/common/optional/mutable/package-info.java
@@ -19,6 +19,13 @@
  *
  * @see com.sogeor.framework.common
  * @see com.sogeor.framework.common.optional.mutable.Mutable
+ * @see com.sogeor.framework.common.optional.mutable.MutableBoolean
+ * @see com.sogeor.framework.common.optional.mutable.MutableByte
+ * @see com.sogeor.framework.common.optional.mutable.MutableDouble
+ * @see com.sogeor.framework.common.optional.mutable.MutableFloat
+ * @see com.sogeor.framework.common.optional.mutable.MutableInteger
+ * @see com.sogeor.framework.common.optional.mutable.MutableLong
+ * @see com.sogeor.framework.common.optional.mutable.MutableShort
  * @since 1.0.0-RC1
  */
 package com.sogeor.framework.common.optional.mutable;
