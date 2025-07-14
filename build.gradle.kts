3bc99848f2ba51efdda8606b90a797ae6b9d9d6b
George <bloogefest@gmail.com>
2025-07-14 16:56:41 +0300

    Изменение файла `build.gradle.kts`
    Signed-off-by: George <bloogefest@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,207 @@
+import com.sogeor.framework.gradle.loadProperty
+import java.net.HttpURLConnection
+import java.net.URI
+import java.util.*
+
 plugins {
-    id("java")
+    `java-platform`
+    `maven-publish`
+    signing
 }
 
-group = "com.sogeor"
+description = "Предоставляет BOM модульного многофункционального фреймворка."
+group = "com.sogeor.framework"
 version = "1.0.0-SNAPSHOT"
 
-subprojects {
-    project.afterEvaluate {
-        group = rootProject.group
-        version = rootProject.version
+dependencies {
+    constraints {
+        api(project(":annotation"))
+        api(project(":collection"))
+        api(project(":common"))
+        api(project(":function"))
+        api(project(":throwable"))
+        api(project(":validation"))
+    }
+}
 
-        java {
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
+publishing {
+    publications {
+        create<MavenPublication>("platform") {
+            artifactId = "bom"
 
-            withJavadocJar()
-            withSourcesJar()
+            from(components["javaPlatform"])
+
+            pom {
+                name = artifactId
+                description = project.description
+                url = "https://github.com/sogeor/framework"
+            }
         }
+    }
+}
 
+tasks.wrapper {
+    gradleVersion = "8.14"
+}
+
+val isSnapshotVersion = "-SNAPSHOT" in version.toString()
+val sonatypeUsername = loadProperty("SONATYPE_CREDENTIALS_USERNAME", rootProject)
+val sonatypePassword = loadProperty("SONATYPE_CREDENTIALS_PASSWORD", rootProject)
+
+allprojects {
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    publishing {
         repositories {
-            mavenCentral()
-        }
+            maven {
+                name = "sonatype"
+                url = uri(
+                    if (isSnapshotVersion) "https://central.sonatype.com/repository/maven-snapshots/"
+                    else "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+                )
 
-        tasks.compileJava {
-            options.encoding = "UTF-8"
-        }
-
-        tasks.javadoc {
-            options.encoding = "UTF-8"
-            val options = options as CoreJavadocOptions
-            options.addStringOption("Xdoclint", "all")
-            options.addStringOption("tag", "apiNote:a:\"API Note:\"")
-            options.addStringOption("tag", "implSpec:a:\"Implementation Requirements:\"")
-            options.addStringOption("tag", "implNote:a:\"Implementation Note:\"")
-        }
-
-        if (name !in arrayOf("annotation", "throwable")) {
-            dependencies {
-                testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-                testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
-            }
-
-            tasks.compileTestJava {
-                options.encoding = "UTF-8"
-            }
-
-            tasks.test {
-                useJUnitPlatform()
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
             }
         }
+
+        publications {
+            withType<MavenPublication> {
+                pom {
+                    description = project.description
+                    url = "https://github.com/sogeor/framework"
+                    inceptionYear = "2025"
+
+                    licenses {
+                        license {
+                            name = "Apache License, Version 2.0"
+                            url = "https://www.apache.org/licenses/LICENSE-2.0"
+                            distribution = "repo"
+                        }
+                    }
+
+                    organization {
+                        name = "Sogeor"
+                        url = "https://github.com/sogeor"
+                    }
+
+                    developers {
+                        developer {
+                            id = "Bloogefest"
+                            name = "George Sopin"
+                            url = "https://github.com/Bloogefest"
+                            organization = "Sogeor"
+                            organizationUrl = "https://github.com/sogeor"
+                            timezone = "Europe/Moscow"
+                        }
+                    }
+
+                    scm {
+                        connection = "scm:git:git://github.com/sogeor/framework.git"
+                        developerConnection = "scm:git:ssh://github.com:sogeor/framework.git"
+                        url = "https://github.com/sogeor/framework"
+                    }
+
+                    issueManagement {
+                        system = "Github"
+                        url = "https://github.com/sogeor/framework/issues"
+                    }
+
+                    ciManagement {
+                        system = "Github"
+                        url = "https://github.com/sogeor/framework/actions"
+                    }
+                }
+            }
+        }
+    }
+
+    signing {
+        useInMemoryPgpKeys(
+            loadProperty("SINGING_KEY_ID", rootProject),
+            loadProperty("SINGING_KEY_SECRET", rootProject),
+            loadProperty("SINGING_KEY_PASSWORD", rootProject)
+        )
+
+        sign(publishing.publications)
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "java-library")
+
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    configure<JavaPluginExtension> {
+        withJavadocJar()
+        withSourcesJar()
+
+        modularity.inferModulePath = true
+
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    if (name !in arrayOf("annotation", "throwable")) tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<Javadoc> {
+        val options = options as CoreJavadocOptions
+        options.encoding = "UTF-8"
+        options.quiet()
+        options.addBooleanOption("Xdoclint:all,-reference", true)
+        options.addMultilineStringsOption("tag").value =
+            listOf("apiNote:a:API Note:", "implSpec:a:Implementation Requirements:", "implNote:a:Implementation Note:")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>(name) {
+                from(components["java"])
+
+                pom {
+                    name = project.name
+                }
+            }
+        }
+    }
+
+    tasks.register("notifySonatypeRepository") {
+        if (isSnapshotVersion) return@register
+
+        val connection =
+            (URI("https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/com.sogeor.framework").toURL()
+                .openConnection() as HttpURLConnection)
+        connection.requestMethod = "POST"
+        connection.setRequestProperty(
+            "Authorization", "Basic ${
+                Base64.getEncoder().encodeToString("${sonatypeUsername}:${sonatypePassword}".toByteArray())
+            }"
+        )
+
+        connection.connect()
+        println("Response (${connection.responseCode}): ${connection.responseMessage}")
+    }
+
+    tasks.register("publishAndNotify") {
+        dependsOn("publish")
+        dependsOn("notifySonatypeRepository")
+        tasks.findByName("notifySonatypeRepository")?.mustRunAfter("publish")
     }
 }
