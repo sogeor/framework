542bdd842211a3a85fef963f76400d76dea2e902
George <bloogefest@gmail.com>
2025-08-11 00:40:30 +0300

    Введение интерфейса `CharacterToCharacterHandler`

---
+++ function/src/main/java/com/sogeor/framework/function/CharacterToCharacterHandler.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2025 Sogeor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sogeor.framework.function;
+
+import com.sogeor.framework.annotation.Contract;
+import com.sogeor.framework.annotation.NonNull;
+import com.sogeor.framework.validation.NullValidationFault;
+import com.sogeor.framework.validation.ValidationFault;
+import com.sogeor.framework.validation.Validator;
+
+/**
+ * Представляет собой обработчик значений типа {@code char}.
+ *
+ * @param <F> тип программного сбоя или неисправности, возникающей при неудачной обработке или возврате значений типа
+ * {@code char}.
+ *
+ * @since 1.0.0-RC1
+ */
+@FunctionalInterface
+public interface CharacterToCharacterHandler<F extends Throwable> {
+
+    /**
+     * Создаёт обработчик значений типа {@code char} со следующей реализацией метода {@code handle(char)}:
+     * <pre>
+     * {@code
+     * value -> value;
+     * }
+     * </pre>
+     *
+     * @param <F> тип программного сбоя или неисправности, возникающей при неудачной обработке или возврате значений
+     * типа {@code char} новым обработчиком.
+     *
+     * @return Новый обработчик значений типа {@code char}
+     *
+     * @see #handle(char)
+     * @since 1.0.0-RC1
+     */
+    @Contract("-> new")
+    static <F extends Throwable> @NonNull CharacterToCharacterHandler<F> empty() {
+        return value -> value;
+    }
+
+    /**
+     * Создаёт обработчик значений типа {@code char} со следующей реализацией метода {@code handle(char)}:
+     * <pre>
+     * {@code
+     * ignored -> value;
+     * }
+     * </pre>
+     *
+     * @param value значение типа {@code char}, возвращаемое новым обработчиком.
+     * @param <F> тип программного сбоя или неисправности, возникающей при неудачной обработке или возврате значений
+     * типа {@code char} новым обработчиком.
+     *
+     * @return Новый обработчик значений типа {@code char}
+     *
+     * @see #handle(char)
+     * @since 1.0.0-RC1
+     */
+    @Contract("? -> new")
+    static <F extends Throwable> @NonNull CharacterToCharacterHandler<F> direct(final char value) {
+        return ignored -> value;
+    }
+
+    /**
+     * Возвращает {@code handler}.
+     *
+     * @param handler обработчик значений типа {@code char}.
+     * @param <F> тип программного сбоя или неисправности, возникающей при неудачной обработке или возврате значений
+     * типа {@code char} {@code handler}.
+     *
+     * @return {@code handler}.
+     *
+     * @apiNote Предназначен для удобного создания {@code handler} на основе лямбда-выражений.
+     * @since 1.0.0-RC1
+     */
+    @Contract("? -> 1")
+    static <F extends Throwable> @NonNull CharacterToCharacterHandler<F> of(
+            final @NonNull CharacterToCharacterHandler<F> handler) {
+        return handler;
+    }
+
+    /**
+     * Обрабатывает {@code value} с помощью этого обработчика и возвращает значение типа {@code char}.
+     *
+     * @param value значение типа {@code char}, обрабатываемое этим обработчиком.
+     *
+     * @return Возвращаемое значение типа {@code char}.
+     *
+     * @throws ValidationFault неудачная валидация, предположительно, {@code value} и/или возвращаемого значения типа
+     * {@code char}.
+     * @throws F неудачная обработка {@code value} или возврат значения типа {@code char}.
+     * @since 1.0.0-RC1
+     */
+    @Contract("? -> ?")
+    char handle(final char value) throws ValidationFault, F;
+
+    /**
+     * Создаёт обработчик значений типа {@code char} со следующей реализацией метода {@code handle(char)}:
+     * <pre>
+     * {@code
+     * value -> handler.handle(handle(value));
+     * }
+     * </pre>
+     *
+     * @param handler обработчик значений типа {@code char}.
+     *
+     * @return Новый обработчик значений типа {@code char}
+     *
+     * @throws ValidationFault неудачная валидация.
+     * @throws NullValidationFault {@code handler} не должен быть {@code null}.
+     * @see #handle(char)
+     * @since 1.0.0-RC1
+     */
+    @Contract("!null -> new; null -> fault")
+    default @NonNull CharacterToCharacterHandler<F> and(
+            final @NonNull CharacterToCharacterHandler<? extends F> handler) throws ValidationFault {
+        Validator.nonNull(handler, "The passed handler");
+        return value -> handler.handle(handle(value));
+    }
+
+    /**
+     * Создаёт обработчик значений типа {@code char} со следующей реализацией метода {@code handle(char)}:
+     * <pre>
+     * {@code
+     * value -> {
+     *     try {
+     *         return handle(value);
+     *     } catch (final @NonNull Throwable primary) {
+     *         try {
+     *             return handler.handle(value);
+     *         } catch (final @NonNull Throwable secondary) {
+     *             primary.addSuppressed(secondary);
+     *             throw primary;
+     *         }
+     *     }
+     * };
+     * }
+     * </pre>
+     *
+     * @param handler обработчик значений типа {@code char}.
+     *
+     * @return Новый обработчик значений типа {@code char}
+     *
+     * @throws ValidationFault неудачная валидация.
+     * @throws NullValidationFault {@code handler} не должен быть {@code null}.
+     * @see #handle(char)
+     * @since 1.0.0-RC1
+     */
+    @Contract("!null -> new; null -> fault")
+    default @NonNull CharacterToCharacterHandler<F> or(
+            final @NonNull CharacterToCharacterHandler<? extends F> handler) throws ValidationFault {
+        Validator.nonNull(handler, "The passed handler");
+        return value -> {
+            try {
+                return handle(value);
+            } catch (final @NonNull Throwable primary) {
+                try {
+                    return handler.handle(value);
+                } catch (final @NonNull Throwable secondary) {
+                    primary.addSuppressed(secondary);
+                    throw primary;
+                }
+            }
+        };
+    }
+
+}
