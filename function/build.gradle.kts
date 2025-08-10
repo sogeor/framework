cffdfae81840402e2e504f49c11281d331bc79d7
George <bloogefest@gmail.com>
2025-08-11 00:37:47 +0300

    Изменение файла `function/build.gradle.kts`

--- function/build.gradle.kts
+++ function/build.gradle.kts
@@ -6,5 +6,6 @@ dependencies {
     implementation(project(":annotation"))
 
     testImplementation(platform("org.junit:junit-bom:${rootProject.ext["junit"]}"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
 }
