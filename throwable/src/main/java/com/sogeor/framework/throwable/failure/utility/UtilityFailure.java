c12ba41ae27d5e98bd78e784948da46f2e17f057
George <bloogefest@gmail.com>
2025-10-09 17:30:37 +0300

    Изменение сбоя `UtilityFailure`

--- throwable/src/main/java/com/sogeor/framework/throwable/failure/utility/UtilityFailure.java
+++ throwable/src/main/java/com/sogeor/framework/throwable/failure/utility/UtilityFailure.java
@@ -29,9 +29,7 @@ import com.sogeor.framework.throwable.failure.CheckedFailure;
 public class UtilityFailure extends CheckedFailure {
 
     /**
-     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, {@linkplain #DEFAULT_CAUSE причиной возникновения},
-     * параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по
-     * умолчанию.
+     * Создаёт экземпляр по умолчанию.
      *
      * @since 1.0.0-RC1
      */
@@ -39,9 +37,7 @@ public class UtilityFailure extends CheckedFailure {
     public UtilityFailure() {}
 
     /**
-     * Создаёт экземпляр с {@linkplain #DEFAULT_CAUSE причиной возникновения}, параметрами
-     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
-     * а также с {@code message}.
+     * Создаёт экземпляр на основе {@code message}.
      *
      * @param message сообщение.
      *
@@ -53,9 +49,7 @@ public class UtilityFailure extends CheckedFailure {
     }
 
     /**
-     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением}, параметрами
-     * {@linkplain #DEFAULT_SUPPRESSION подавления} и {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию,
-     * а также с {@code cause}.
+     * Создаёт экземпляр на основе {@code cause}.
      *
      * @param cause причина возникновения.
      *
@@ -67,8 +61,7 @@ public class UtilityFailure extends CheckedFailure {
     }
 
     /**
-     * Создаёт экземпляр с параметрами {@linkplain #DEFAULT_SUPPRESSION подавления} и
-     * {@linkplain #DEFAULT_STACK_TRACE трассировки стека} по умолчанию, а также с {@code message} и {@code cause}.
+     * Создаёт экземпляр на основе {@code message} и {@code cause}.
      *
      * @param message сообщение.
      * @param cause причина возникновения.
@@ -78,20 +71,6 @@ public class UtilityFailure extends CheckedFailure {
     @Contract("?, ? -> new")
     public UtilityFailure(final @Nullable String message, final @Nullable Throwable cause) {
         super(message, cause);
-    }
-
-    /**
-     * Создаёт экземпляр с {@linkplain #DEFAULT_MESSAGE сообщением} и {@linkplain #DEFAULT_CAUSE причиной возникновения}
-     * по умолчанию, а также с {@code suppression} и {@code stackTrace}.
-     *
-     * @param suppression параметр подавления.
-     * @param stackTrace параметр трассировки стека.
-     *
-     * @since 1.0.0-RC1
-     */
-    @Contract("?, ? -> new")
-    public UtilityFailure(final boolean suppression, final boolean stackTrace) {
-        super(suppression, stackTrace);
     }
 
     /**
