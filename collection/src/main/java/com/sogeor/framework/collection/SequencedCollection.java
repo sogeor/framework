4af6cbd464a0ad7c72bc29278a9a0f3c79540689
George <bloogefest@gmail.com>
2025-10-12 17:02:32 +0300

    Изменение интерфейса `SequencedCollection`

--- collection/src/main/java/com/sogeor/framework/collection/SequencedCollection.java
+++ collection/src/main/java/com/sogeor/framework/collection/SequencedCollection.java
@@ -17,97 +17,24 @@
 package com.sogeor.framework.collection;
 
 import com.sogeor.framework.annotation.Contract;
-import com.sogeor.framework.annotation.NonNull;
 
 /**
  * Представляет собой упорядоченную коллекцию элементов.
  *
- * @param <T> тип элементов.
- *
- * @see Iterator
  * @since 1.0.0-RC1
  */
-public interface SequencedCollection<T> extends IterableCollection<T> {
+public interface SequencedCollection extends Collection {
 
     /**
-     * @return Новый итератор элементов этой коллекции.
-     *
-     * @implSpec Возвращаемый итератор должен находится в неопределённом состоянии.
-     * @implNote Ожидаемая реализация обладает оценкой временной сложности {@code Θ(1)}.
-     * @see Iterator
-     * @since 1.0.0-RC1
-     */
-    @Override
-    @Contract("-> new")
-    @NonNull
-    Iterator<T> iterator();
-
-    /**
-     * Если {@code empty()}, то возвращает {@code 1}, иначе вычисляет хеш-код этой коллекции на основе её элементов и
+     * Если {@link #empty()}, то возвращает {@code 1}, иначе вычисляет хеш-код на основе элементов этой коллекции и
      * возвращает его.
      *
-     * @return Хеш-код этой коллекции.
+     * @return Если {@link #empty()}, то {@code 1}, иначе хеш-код на основе элементов этой коллекции.
      *
-     * @implNote Ожидаемая реализация обладает оценками временной сложности {@code Ω(1)} и {@code O(n)}.
      * @since 1.0.0-RC1
      */
     @Override
     @Contract("-> value")
     int hashCode();
-
-    /**
-     * Представляет собой итератор элементов упорядоченной коллекции.
-     *
-     * @param <T> тип элементов.
-     *
-     * @see SequencedCollection
-     * @since 1.0.0-RC1
-     */
-    interface Iterator<T> extends IterableCollection.Iterator<T> {
-
-        /**
-         * Если {@code exists(index)}, то переходит к элементу по {@code index} и возвращает {@code true}, иначе —
-         * {@code false}.
-         *
-         * @param index индекс элемента.
-         *
-         * @return {@code true} или {@code false}.
-         *
-         * @implNote Стандартная реализация обладает оценками временной сложности {@code Ω(1)} и {@code O(n²)}.
-         * @see #exists(long)
-         * @since 1.0.0-RC1
-         */
-        @SuppressWarnings("StatementWithEmptyBody")
-        @Contract("? -> value")
-        default boolean move(final long index) {
-            if (!exists(index)) return false;
-            while (index > index() && next()) ;
-            while (index < index() && previous()) ;
-            return true;
-        }
-
-        /**
-         * Если элемент по {@code index} существует, то возвращает {@code true}, иначе — {@code false}.
-         *
-         * @param index индекс элемента.
-         *
-         * @return {@code true} или {@code false}.
-         *
-         * @implNote Ожидаемая реализация обладает оценками временной сложности {@code Ω(1)} и {@code O(n)}.
-         * @since 1.0.0-RC1
-         */
-        @Contract("? -> value")
-        boolean exists(final long index);
-
-        /**
-         * @return Если {@code current()}, то индекс текущего элемента, иначе {@code -1}.
-         *
-         * @see #current()
-         * @since 1.0.0-RC1
-         */
-        @Contract("-> value")
-        long index();
-
-    }
 
 }
